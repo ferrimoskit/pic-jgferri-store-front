0a7613790d79a91d4fc4489328579bac132aeebb
Ferri <ferri@moskit.com.br>
2024-11-24 16:43:45 -0300

    feat: home added

---
+++ src/components/views/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MessageOfTheDay from "../messageOfTheDay/messageOfTheDay";
+import ProductCarrousel from "../product/ProductCarrousel";
+const Home = () => {
+  return (
+    <>
+      <MessageOfTheDay />
+      <ProductCarrousel />
+    </>
+  );
+};
+
+export default Home;
