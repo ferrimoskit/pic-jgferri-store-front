734e0cc52c776ab9b0fef116bbc1dae586d594eb
Ferri <ferri@moskit.com.br>
2024-11-24 16:43:27 -0300

    feat: motd added

---
+++ src/components/messageOfTheDay/messageOfTheDay.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MessageOfTheDay = () => {
+  return (
+    <container className="mt-32 flex justify-center">
+      <h2 className="text-7xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+    </container>
+  );
+};
+
+export default MessageOfTheDay;
