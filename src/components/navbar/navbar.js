24cce5835628738f42c755fd39dd82dfc4727fa3
Ferri <ferri@moskit.com.br>
2024-11-24 20:17:25 -0300

    !feat: added CartProvider

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Logo from "./logoipsum-329.svg";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <header className="flex flex-row flex-start flex-nowrap h-12">
       <div className="flex flex-1 justify-items-start items-center">
         <div>
-          <img src={Logo} alt="Logo" className="w-20 px-5 py-1" />
+          <Link to="/"><img src={Logo} alt="Logo" className="w-20 px-5 py-1" /> </Link>
         </div>
       </div>
       <div className="flex-1 flex justify-items-start flex-row items-center">
@@ -13,7 +14,7 @@ const Navbar = () => {
           <p className="px-5 text-lg">Contato</p>
         </div>
         <div>
-          <p className="px-5 text-lg">Carrinho</p>
+           <p className="px-5 text-lg"><Link to="/carrinho">Carrinho</Link></p>
         </div>
       </div>
     </header>
