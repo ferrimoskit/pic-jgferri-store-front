376df235a496947e17aa314721dfc8776f79d487
Ferri <ferri@moskit.com.br>
2024-11-29 21:48:24 -0300

    feat: Admin, Login routes added

--- src/Routes.js
+++ src/Routes.js
@@ -4,14 +4,18 @@ import Home from "./components/views/home";
 import ProductDetails from "./components/product/ProductDetails";
 import Cart from "./components/cart/Cart";
 import Navbar from "./components/navbar/navbar";
+import Login from "./components/views/Login";
+import Admin from "./components/views/Admin";
 const Routes = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <ReactRoutes>
         <Route element={<Home />} path="/" />
-        <Route element={<ProductDetails />} path="/product/:id"/>
-        <Route element={<Cart />} path="/carrinho"/>
+        <Route element={<ProductDetails />} path="/product/:id" />
+        <Route element={<Cart />} path="/carrinho" />
+        <Route element={<Login />} path="/login" />
+        <Route element={<Admin />} path="/admin" />
       </ReactRoutes>
     </BrowserRouter>
   );
